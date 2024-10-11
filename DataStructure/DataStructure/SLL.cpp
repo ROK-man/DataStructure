03864e28c4921e9dc524b94440367157f6a4cbde
Laptop <hwangjs0823@pusan.ac.kr>
2024-10-12 05:43:19 +0900

    practice

--- DataStructure/DataStructure/SLL.cpp
+++ DataStructure/DataStructure/SLL.cpp
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+typedef struct Node {
+	int data;
+}Node;
+typedef struct SLL {
+	Node head;
+	Node* nextNode;
+}SLL;
 int main()
 {
-	printf("Hello Git");
+	SLL sll;
+	Node n; n.data = 1;
+	sll.head = n;
+
+	printf("%d", sll.head.data);
+
+	Node n2; n2.data = 2;
+	sll.nextNode = &n2;
+
+	printf("%d", sll.nextNode->data);
+
+	return 0;
 }

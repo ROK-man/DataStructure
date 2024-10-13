e8d8083d0e3f3e6a8142d1e5d753643423e246af
ROK-man <ha.jinseong0.0@gmail.com>
2024-10-13 15:51:56 +0900

    CDLL_ver0.2

--- DataStructure/DataStructure/CDLL.h
+++ DataStructure/DataStructure/CDLL.h
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 typedef struct CDLL_Node
 {
-	int data;
+	int Data;
 	CDLL_Node* Next;
 	CDLL_Node* Previous;
 }CDLL_Node;
@@ -25,13 +25,21 @@ CDLL* CDLL_Init()
 	return list;
 }
 
+// 리스트 비었는지 확인
+int CDLL_IsListEmpty(CDLL* list)
+{
+	if (list->Size == 0)	return 1;
+	return -1;
+}
+
+
 // 데이터 추가
 void CDLL_AddData(CDLL* list, int i)
 {
 	CDLL_Node* new_node = (CDLL_Node*)malloc(sizeof(CDLL_Node));
 	if (new_node == NULL) return;
 
-	new_node->data = i;
+	new_node->Data = i;
 	list->Size++;
 
 	if (list->Head == NULL)
@@ -55,13 +63,16 @@ void CDLL_AddData(CDLL* list, int i)
 // 전체 출력
 void CDLL_ViewAllData(CDLL* list)
 {
-	if (list->Head == NULL) return;
-
+	if (list->Head == NULL)
+	{
+		printf("list is empty\n");
+		return;
+	}
 	CDLL_Node* current_node = list->Head;
 
 	do
 	{
-		printf("%d -> ", current_node->data);
+		printf("%d -> ", current_node->Data);
 		current_node = current_node->Next;
 	} while (current_node != list->Head);
 	printf("\n");
@@ -79,7 +90,7 @@ int CDLL_FindData(CDLL* list, int data)
 	int index = 0;
 	do
 	{
-		if (current_node->data == data)
+		if (current_node->Data == data)
 		{
 			return index;
 		}
@@ -89,9 +100,11 @@ int CDLL_FindData(CDLL* list, int data)
 
 	return -1;
 }
+
+// 리스트 크기 반환
 int CDLL_GetSize(CDLL* list)
 {
-	if (list == NULL || list->Head == NULL)
+	if (list == NULL)
 	{
 		return -1;
 	}
@@ -108,7 +121,7 @@ int CDLL_RemoveData(CDLL* list, int data)
 	int count = 0;
 	do
 	{
-		if (current_node->data == data)
+		if (current_node->Data == data)
 		{
 			if (list->Size == 1)
 			{
@@ -145,7 +158,85 @@ int CDLL_RemoveData(CDLL* list, int data)
 	return -1;
 }
 
+// 특정 인덱스에 있는 데이터 삭제 후 해당 데이터 값 반환, 실패시 -1 반환
+int CDLL_RemoveDataAt(CDLL* list, int index)
+{
+	if (list->Head == NULL)
+	{
+		return -1;
+	}
+	if (CDLL_GetSize(list)-1 < index)
+	{
+		return -1;
+	}
 
+	CDLL_Node* current_node = list->Head;
+
+	if (index == 0)
+	{
+		int data = current_node->Data;
+
+		if (CDLL_GetSize(list) == 1)
+		{
+			free(current_node);
+			list->Head = NULL;
+			list->Size = 0;
+
+			return data;
+		}
+
+		list->Head = current_node->Next;
+		list->Head->Previous = current_node->Previous;
+		current_node->Previous->Next = list->Head;
+
+		free(current_node);
+		list->Size--;
+
+		return data;
+	}
+	do
+	{
+		if (index == 0)
+		{
+			current_node->Previous->Next = current_node->Next;
+			current_node->Next->Previous = current_node->Previous;
+			
+			int data = current_node->Data;
+			free(current_node);
+			list->Size--;
+
+			return data;
+		}
+
+		index--;
+		current_node = current_node->Next;
+
+	} while (current_node != list->Head);
+
+	return -1;
+}
+
+// 리스트 삭제
+int CDLL_Destroy(CDLL* list)
+{
+	if (list == NULL)
+	{
+		return -1;
+	}
+
+	while (CDLL_GetSize(list) > 0)
+	{
+		CDLL_RemoveDataAt(list, 0);
+		// CDLL_ViewAllData(list);
+	}
+
+	if (list->Head == NULL)
+	{
+		free(list);
+		return 1;
+	}
+	return -1;
+}
 
 // 샘플 코드
 void CDLL_Sample()
@@ -183,6 +274,9 @@ void CDLL_Sample()
 
 	printf("리스트 크기\n");
 	printf("%d\n", CDLL_GetSize(list));
+
+	printf("리스트 삭제\n");
+	printf("%d\n", CDLL_Destroy(list));
 
 	return;
 }
